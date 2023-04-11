038b50a6a8e3cbab5a4d35388cebecd56e1f8d80
Jake Imber <j8keimber@gmail.com>
2023-04-11 14:52:55 +0100

    Changed varibles to camel case.

--- wordstrip.c
+++ wordstrip.c
@@ -1,5 +1,3 @@
-// NEED SOME CAMEL CASE UP IN THIS BITCH
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,38 +7,38 @@
 #define MAX_SENTENCE_LENGTH 9999
 
 int main(int argc, char *argv[]) {
-    FILE *input_file, *output_file;
-    char *input_filename, *output_filename;
+    FILE *inputFile, *outputFile;
+    char *inputFileName, *outputFileName;
     char sentence[MAX_SENTENCE_LENGTH];
     char *token;
     // Potential probelms here, notably full stops at the end of speaking.
     const char *delimiter = ".!?";
-    int sentence_count = 0, selected_count = 0, percentileSelectionChance = 75, writtenSentences = 0;
+    int sentenceCount = 0, selectedCount = 0, percentileSelectionChance = 75, writtenSentences = 0;
     int i;
 
     // Check if input and output filenames are provided
     if (argc < 3) {
-        printf("Usage: %s input_filename output_filename\n", argv[0]);
+        printf("Usage: %s inputFileName outputFileName\n", argv[0]);
         return 1;
     }
 
     // Useful to know for later, how to do it with hyphon prefix?
-    input_filename = argv[1];
-    output_filename = argv[2];
-    // percentile_selection_change = 100-arg[n];
+    inputFileName = argv[1];
+    outputFileName = argv[2];
+    // percentileSelectionChance = 100-arg[n];
 
     // Open input and output files
     // This needs to work for .odt files as well.
-    input_file = fopen(input_filename, "r+");
-    if (!input_file) {
-        printf("Error opening input file: %s\n", input_filename);
+    inputFile = fopen(inputFileName, "r+");
+    if (!inputFile) {
+        printf("Error opening input file: %s\n", inputFileName);
         return 1;
     }
 
-    output_file = fopen(output_filename, "w");
-    if (!output_file) {
-        printf("Error opening output file: %s\n", output_filename);
-        fclose(input_file);
+    outputFile = fopen(outputFileName, "w");
+    if (!outputFile) {
+        printf("Error opening output file: %s\n", outputFileName);
+        fclose(inputFile);
         return 1;
     }
 
@@ -48,14 +46,14 @@ int main(int argc, char *argv[]) {
     srand(time(NULL));
     // Still randomly writes empty lines, why?
     // Empty lines can still be included with current delims, 
-    while (fgets(sentence, MAX_SENTENCE_LENGTH, input_file)) {
+    while (fgets(sentence, MAX_SENTENCE_LENGTH, inputFile)) {
         token = strtok(sentence, delimiter);
         while (token != NULL) {
             // RNG, probably a cleaner way to do this. Wich condition is read first?
             if (rand() % 101 >= percentileSelectionChance && strchr(token, '\n') == NULL){ //I think the para filter actually works?! Probably really slow.
                 // Whitespace filter, probably a quicker way to do this
                 while(isspace((unsigned char)*token)) token++;
-                fprintf(output_file, "%s\n", token);
+                fprintf(outputFile, "%s\n", token);
                 writtenSentences++;
             }
             token = strtok(NULL, delimiter);
@@ -63,8 +61,8 @@ int main(int argc, char *argv[]) {
     }
 
     
-    fclose(input_file);
-    fclose(output_file);
+    fclose(inputFile);
+    fclose(outputFile);
 
     printf("Wrote %d sentences \n", writtenSentences);
 
